04d67e1e320c916c33d0b12b84bebe45a0016722
dleniston <dleniston@tssg.org>
2023-03-07 10:42:49 +0000

    Added upcoming movies page.

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import SiteHeader from './components/siteHeader'
 import HomePage from "./pages/homePage";
+import UpcomingMovies from "./pages/upcomingMovies";
 import MoviePage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
@@ -12,6 +13,7 @@ const App = () => {
     <BrowserRouter>
         <SiteHeader />      {/* New Header  */}
       <Routes>
+        <Route path="/movies/upcoming" element={<UpcomingMovies />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
