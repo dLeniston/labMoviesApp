d4c4b4c2c8244e06cb32316a43fcebba746248d7
dleniston <dleniston@tssg.org>
2023-03-13 12:48:25 +0000

    Added new review support.

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -8,6 +8,7 @@ import HomePage from "./pages/homePage";
 import UpcomingMovies from "./pages/upcomingMovies";
 import MoviePage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MoviesContextProvider from "./contexts/moviesContext";
 
@@ -30,6 +31,7 @@ const App = () => {
             <Routes>
               <Route path="/movies/upcoming" element={<UpcomingMovies />} />
               <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+              <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
               <Route path="/movies/:id" element={<MoviePage />} />
               <Route path="/reviews/:id" element={<MovieReviewPage/>} />
               <Route path="/" element={<HomePage />} />
