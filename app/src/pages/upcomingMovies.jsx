43edb5e2148804c214b37a0d8b019f2be0cb1b91
dleniston <dleniston@tssg.org>
2023-03-13 17:37:15 +0000

    Fix upcoming movies page.

--- app/src/pages/upcomingMovies.jsx
+++ app/src/pages/upcomingMovies.jsx
@@ -1,21 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Spinner from "../components/spinner";
 import PageTemplate from '../components/templateMovieListPage'
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import { useQuery } from "react-query";
 import { getUpcomingMovies } from "../api/tmdb-api";
 
 const UpcomingMovies = (props) => {
-  const [movies, setMovies] = useState([]);
+  //const [movies, setMovies] = useState([]);
+  const { data, error, isLoading, isError } = useQuery("upcoming", getUpcomingMovies);
 
-  useEffect(() => {
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  /*useEffect(() => {
     getUpcomingMovies().then(movies => {
       setMovies(movies);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, []);*/
+  const movies = data ? data.results : [];
 
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
   );
 };
