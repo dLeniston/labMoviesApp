0ee8dc5fc067936f39eebe97cfec538cb836fb56
dleniston <dleniston@tssg.org>
2023-04-08 16:01:43 +0100

    Replaced MUI imageList with autoplay image carousel for movie details page

--- app/src/pages/homePage.jsx
+++ app/src/pages/homePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
   const { filterValues, setFilterValues, filterFunction } = useFiltering([],[titleFiltering, genreFiltering]);
   const { session }  = useAuth();
   const [currPage, setCurrPage] = useState(1);
-  const [recsPerPage] = useState(12);
+  const [recsPerPage] = useState(6);
   const pages = [1,2,3,4,5,6,7,8,9,10];
 
   // Create array of queries to get pages of movies in parallel.
